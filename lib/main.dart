d91d0bfb8043b7b145c8d8ce9b520592e1403489
Barry3232 <davelanista@gmail.com>
2026-08-12 20:06:47 +0100

    removed unused import.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:locafy/features/screens/splash_screen.dart';
-import 'package:firebase_app_check/firebase_app_check.dart';
 import 'firebase_options.dart';
 
 void main() async {
